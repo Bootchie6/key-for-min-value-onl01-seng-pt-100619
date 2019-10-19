dd7bcb4ce18a94798ff3076df8fd408515fafd3d
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-19 22:11:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_object = nil
   name_hash.each do |object, value|
-    if value < min_value
+    if min_value == value < min_value
       min_value = value
       min_object = object
     end
